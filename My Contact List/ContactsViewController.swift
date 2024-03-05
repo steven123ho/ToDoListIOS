b98157d6e93fdeba98b5b0d2836771a3fd17f709
steven123ho <steven123ho@gmail.com>
2024-03-05 15:57:10 -0500

    Testing Xcode commit and push to github

--- My Contact List/ContactsViewController.swift
+++ My Contact List/ContactsViewController.swift
@@ -12,7 +12,7 @@ class ContactsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view. Hello
     }
     
 
