80edf605cf09b93a5960aab789976c4eba987917
steven123ho <steven123ho@gmail.com>
2024-03-21 10:40:52 -0400

    settings UI finished

--- My Contact List/SettingsViewController.swift
+++ My Contact List/SettingsViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pckSortField: UIPickerView!
+    @IBOutlet weak var swAscending: UISwitch!
+    
+    let sortOrderItems: Array<String> = ["ContactName", "City", "Birthday"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pckSortField.dataSource = self;
+        pckSortField.delegate = self;
     }
+    
+    
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+    }
+    
+    // MARK: UIPickerViewDelegate Methods
+    
+    // returns the number of 'columns' to display
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // Returns the number of rows in the picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sortOrderItems.count
+    }
+    
+    // Sets the value that is shown for each row in the picker
+    func pickerView (_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sortOrderItems[row]
+    }
+    
+    //If the user chooses from the pickerview, it calls this function
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Chosen item: \(sortOrderItems[row])")
+    }
+    
     
 
     /*
