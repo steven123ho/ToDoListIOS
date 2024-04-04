4d1ed1e1581314ed88fdfa0dd5c48566e095a132
steven123ho <steven123ho@gmail.com>
2024-04-04 12:07:46 -0400

    Finished majority of map for contacts and for user. Need to implement switching map types

--- My Contact List/LocationDemoViewController.swift
+++ My Contact List/LocationDemoViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreLocation
 
-class LocationDemoViewController: UIViewController {
+class LocationDemoViewController: UIViewController, CLLocationManagerDelegate {
 
     
     @IBOutlet weak var txtStreet: UITextField!
@@ -25,6 +25,7 @@ class LocationDemoViewController: UIViewController {
     @IBOutlet weak var lblAltitudeAccuracy: UILabel!
     
     lazy var geoCoder = CLGeocoder()
+    var locationManager: CLLocationManager!
     
     
     @objc func dismissKeyboard() {
@@ -36,7 +37,21 @@ class LocationDemoViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
         view.addGestureRecognizer(tap)
+        // Set up location manager
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() // ask for permission to use location
+    }
+    
+    // Called when the location permission status changes
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            print("Permission granted")
+        } else {
+            print("Permission NOT granted")
+        }
     }
     
     @IBAction func addressToCoordinates(_ sender: Any) {
@@ -65,8 +80,60 @@ class LocationDemoViewController: UIViewController {
     }
     
     @IBAction func deviceCoordinates(_ sender: Any) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let eventDate = location.timestamp
+            let howRecent = eventDate.timeIntervalSinceNow
+            if Double(howRecent) < 15.0 {
+                let coordinate = location.coordinate
+                lblLongitude.text = String(format: "%g\u{00B0}", coordinate.longitude)
+                lblLatitude.text = String(format: "%g\u{00B0}", coordinate.longitude)
+                lblLocationAccuracy.text = String(format: "%gm", location.horizontalAccuracy)
+                lblAltitude.text = String(format: "%gm", location.altitude)
+                lblAltitudeAccuracy.text = String(format: "%gm", location.verticalAccuracy)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        if newHeading.headingAccuracy > 0 {
+            let theHeading = newHeading.trueHeading
+            var direction: String
+            switch theHeading {
+            case 225..<315:
+                direction = "W"
+            case 135..<225:
+                direction = "S"
+            case 45..<135:
+                direction = "E"
+            default:
+                direction = "N"
+            }
+            lblHeading.text = String(format: "%g\u{00B0} (%@)", theHeading, direction)
+            lblHeadingAccuracy.text = String(format: "%g\u{00B0}", newHeading.headingAccuracy)
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let errorType = error._code == CLError.denied.rawValue ? "Location Permission Denied" : "Unknown Error"
+        let alertController = UIAlertController(title: "Error Getting Location: \(errorType)", message: "Erorr Message: \(error.localizedDescription))", preferredStyle: .alert)
+        let actionOK = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(actionOK)
+        present(alertController, animated: true, completion: nil)
+    }
     
 
     /*
--- My Contact List/MapViewController.swift
+++ My Contact List/MapViewController.swift
@@ -6,16 +6,99 @@
 //
 
 import UIKit
+import MapKit
+import CoreData
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    var contacts: [Contact] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        mapView.delegate = self
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        //Get contacts from core data
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+        var fetchedObjects: [NSManagedObject] = []
+        do {
+            fetchedObjects = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        contacts = fetchedObjects as! [Contact]
+        
+        //remove all annotations
+        let allAnnotations = mapView.annotations
+        mapView.removeAnnotations(allAnnotations)
+        //self.mapView.removeAnnotation(self.mapView.annotations)
+        
+        // Go through all contacts
+        for contact in contacts {// as! [Contacts]
+            let address = "\(contact.streetAddress!), \(contact.city!) \(contact.state!)"
+            // Geocode
+            let geoCoder = CLGeocoder()
+            geoCoder.geocodeAddressString(address) { (placemarks, error) in self.processAddressResponse(contact, withPlacemarks: placemarks, error: error) }
+        }
+    }
+    
+    private func processAddressResponse (_ contact: Contact, withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        if let error = error {
+            print("Geocode Error: \(error)")
+        } else {
+            var bestMatch: CLLocation?
+            if let placemarks = placemarks, placemarks.count > 0 {
+                bestMatch = placemarks.first?.location
+            }
+            if let coordinate = bestMatch?.coordinate {
+                let mp = MapPoint(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                mp.title = contact.contactName
+                mp.subtitle = contact.streetAddress
+                mapView.addAnnotation(mp)
+            } else {
+                print ("Didn't find any matching locations")
+            }
+        }
+    }
+    
+    @IBAction func findUser(_ sender: Any) {
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(.follow, animated: true)
+    }
+    
+    @IBAction func zoomOut(_ sender: Any) {
+        mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        var span = MKCoordinateSpan()
+        span.latitudeDelta = 0.2
+        span.longitudeDelta = 0.2
+        let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, span: span)
+        mapView.setRegion(viewRegion, animated: true)
+        
+        let mp = MapPoint(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        mp.title = "You"
+        mp.subtitle = "Are Here"
+        mapView.addAnnotation(mp)
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
